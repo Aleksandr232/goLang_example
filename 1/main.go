84ccb713dc1ae78a7e5d514b0d718a64d84c4818
Aleksandr232 <aleksrowing232@gmail.com>
2026-06-29 18:09:18 +0300

    first commit

---
+++ 1/main.go
@@ -0,0 +1,41 @@
+package main;
+
+import (
+	"fmt"
+)
+
+func main(){
+	/* var message int;
+	message = 10; */
+	// message := "message";
+	// fmt.Println(reflect.TypeOf(message));
+	
+	// var message string;
+	// var number float32;
+	// var number2 int;
+	// var boolean bool;
+
+	// number = 10.5;
+	// number2 = 10;
+
+
+	// if number > float32(number2) {
+	// 	fmt.Println("ok")
+	// }else{
+	// 	fmt.Println("not ok")
+	// }
+
+	// var message []byte;
+	// message = []byte("message");
+	// fmt.Println(message);
+
+	// var message byte = 62;
+	// fmt.Printf("%c\n", message);
+
+	// var a rune = 'a';
+	// fmt.Println(a);
+
+	// a, b, c := 1, 2, 3;
+	// a, b, c = c, a, b;
+	// fmt.Println(a, b, c);
+}
